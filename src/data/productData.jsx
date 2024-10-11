584e99841fb5711fad567f105691b9764bb8e0f1
Jakub Keklak <keklak.jakub122@gmail.com>
2024-10-11 19:39:48 +0200

    Add product data to src/data/productData.jsx

---
+++ src/data/productData.jsx
@@ -0,0 +1,86 @@
+export const productData = [
+    {
+        name: 'Smrek',
+        price: 2,
+        description: 'Popis',
+        image: 'https://www.krby-kotly.sk/fot'
+    },
+    {
+        name: 'Buk',
+        price: 3,
+        description: 'Popis',
+        image: 'https://www.krby-kotly.sk/fot'
+    },
+    {
+        name: 'Dub',
+        price: 4,
+        description: 'Popis',
+        image: 'https://www.krby-kotly.sk/fot'
+    },
+    {
+        name: 'Jaseň',
+        price: 5,
+        description: 'Popis',
+        image: 'https://www.krby-kotly.sk/fot'
+    },
+    {
+        name: 'Hruška',
+        price: 6,
+        description: 'Popis',
+        image: 'https://www.krby-kotly.sk/fot'
+    },
+    {
+        name: 'Jablon',
+        price: 7,
+        description: 'Popis',
+        image: 'https://www.krby-kotly.sk/fot'
+    },
+    {
+        name: 'Čerešňa',
+        price: 8,
+        description: 'Popis',
+        image: 'https://www.krby-kotly.sk/fot'
+    },
+    {
+        name: 'Slivka',
+        price: 9,
+        description: 'Popis',
+        image: 'https://www.krby-kotly.sk/fot'
+    },
+    {
+        name: 'Orech',
+        price: 10,
+        description: 'Popis',
+        image: 'https://www.krby-kotly.sk/fot'
+    },
+    {
+        name: 'Hrachovka',
+        price: 11,
+        description: 'Popis',
+        image: 'https://www.krby-kotly.sk/fot'
+    },
+    {
+        name: 'Borovica',
+        price: 12,
+        description: 'Popis',
+        image: 'https://www.krby-kotly.sk/fot'
+    },
+    {
+        name: 'Smrek',
+        price: 13,
+        description: 'Popis',
+        image: 'https://www.krby-kotly.sk/fot'
+    },
+    {
+        name: 'Buk',
+        price: 14,
+        description: 'Popis',
+        image: 'https://www.krby-kotly.sk/fot'
+    },
+    {
+        name: 'Dub',
+        price: 15,
+        description: 'Popis',
+        image: 'https://www.krby-kotly.sk/fot'
+    },
+]
