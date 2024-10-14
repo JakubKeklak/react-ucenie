47fa350d5d6e99df2bd869a777789d8a83407f61
Jakub Keklak <keklak.jakub122@gmail.com>
2024-10-14 22:48:43 +0200

    Refactor AnimatedHero component to add animated hero section with image slides and navigation dots

---
+++ src/components/AnimatedHero.jsx
@@ -0,0 +1,61 @@
+import './AnimatedHero.css'
+import { useState, useEffect, useRef } from 'react'
+import Button from './button'
+
+const AnimatedHero = ({ data, buttonVariant, icon }) => {
+    const [activeIndex, setActiveIndex] = useState(0);
+    const intervalRef = useRef(null);
+
+    const handleActiveIndex = (index) => {
+        setActiveIndex(index);
+        resetInterval();
+    }
+
+    const resetInterval = () => {
+        if (intervalRef.current) {
+            clearInterval(intervalRef.current);
+        }
+        intervalRef.current = setInterval(() => {
+            setActiveIndex((prevIndex) => (prevIndex + 1) % data.length);
+        }, 6000); // 6 seconds
+    }
+
+    useEffect(() => {
+        resetInterval();
+        return () => clearInterval(intervalRef.current); // Cleanup interval on component unmount
+    }, [data.length]);
+
+    return (
+        <div className='animated-hero'>
+            {data.map((slide, index) => {
+                return (
+                    <div key={index} className={`animated-hero__card ${activeIndex === index ? 'animated-hero__card--active' : ''}`}>
+                        <div className="animated-hero__card-image">
+                            <img  src={slide.image} alt="" />
+                        </div>
+                        <div className='animated-hero__card-wrapper'>
+                            <div className='animated-hero__card-content'></div>
+                            <div className='animated-hero__card-content'>
+                                <div className='animated-hero__content-left'>
+                                    <h2 className='animated-hero__content-title'>{slide.title}</h2>
+                                    <p className='animated-hero__content-text'>{slide.text}</p>
+                                    <Button text='Viac informacii' url={slide.buttonLink} variant={buttonVariant} icon={icon} />
+                                </div>
+                            </div>
+                            <div className='animated-hero__card-content'></div>
+                        </div>
+                    </div>
+                );
+            })}
+            <div className='animated-hero__dots'>
+                {data.map((_, index) => {
+                    return (
+                        <div key={index} className={`animated-hero__dot ${activeIndex === index ? 'animated-hero__dot--active' : ''}`} onClick={() => handleActiveIndex(index)}></div>
+                    );
+                })}
+            </div>
+        </div>
+    );
+}
+
+export default AnimatedHero;
