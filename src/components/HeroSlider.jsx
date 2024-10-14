a56d6f25aa259fe688e143ada2bc580f08c81ed3
Jakub Keklak <keklak.jakub122@gmail.com>
2024-10-14 22:49:15 +0200

    Add HeroSlider component with autoplay functionality

---
+++ src/components/HeroSlider.jsx
@@ -0,0 +1,13 @@
+import './HeroSlider.css';
+import AutoPlay from './SimpleSlider';
+
+const HeroSlider = () => {
+    return (
+        <div className="hero-slider">
+            <AutoPlay />
+                       
+        </div>
+    );
+}
+
+export default HeroSlider;
