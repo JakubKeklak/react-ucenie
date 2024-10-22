0223df20c46d5be94ea85fffd59c1d15b25f9a2a
Jakub Keklak <keklak.jakub122@gmail.com>
2024-10-22 19:48:38 +0200

    Refactor productData to update product names, prices, and descriptions

--- src/data/productData.jsx
+++ src/data/productData.jsx
@@ -1,18 +1,34 @@
 export const productData = [
     {
         id: 1,
-        name: 'Smrek',
-        price: 2,
-        description: 'Popis',
-        image: 'https://www.krby-kotly.sk/fot',
+        name: 'Smrek, jedla',
+        price: 34,
+        description: 'sypané mäkké drevo',
+        image: 'https://stipanepalivovedrevo.cz/images/virtuemart/product/smrk-sttpane-drevo-mekke-sypane.jpg',
         quantity:1
     },
     {
         id: 2,
-        name: 'Buk',
-        price: 3,
-        description: 'Popis',
-        image: 'https://www.krby-kotly.sk/fot',
+        name: 'Smrek, jedla',
+        price: 42,
+        description: 'ukladané mäkké drevo',
+        image: 'https://www.krby-kotly.sk/fotky47530/fotos/Drewno-bukowe-BUK-OPAL-30-KG.jpg',
+        quantity:1
+    },
+    {
+        id: 3,
+        name: 'Buk, dub',
+        price: 47,
+        description: 'sypané tvrdé drevo',
+        image: 'https://stipanepalivovedrevo.cz/images/virtuemart/product/smrk-sttpane-drevo-mekke-sypane.jpg',
+        quantity:1
+    },
+    {
+        id: 4,
+        name: 'Buk, dub',
+        price: 57,
+        description: 'ukladané tvrdé drevo',
+        image: 'https://www.krby-kotly.sk/fotky47530/fotos/Drewno-bukowe-BUK-OPAL-30-KG.jpg',
         quantity:1
     }
 ]
