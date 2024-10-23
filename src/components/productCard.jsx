a395b4e1ba4dbd01d90863f61b822e9936cbf11d
Jakub Keklak <keklak.jakub122@gmail.com>
2024-10-23 20:41:14 +0200

    Refactor productCard component to display product image and use Button component for adding products

--- src/components/productCard.jsx
+++ src/components/productCard.jsx
@@ -7,13 +7,11 @@ const ProductCard = ({ product, pushProduct }) => {
         <div className="product__card">
             <div className="product__card__description">
                 <h2>{product.name}</h2>
-                <p>Popis</p>
+                <p>Typ: {product.description}</p>
             </div>
             <p>Cena: {product.price}€</p>
-            <img src="https://www.krby-kotly.sk/fotky47530/fotos/Drewno-bukowe-BUK-OPAL-30-KG.jpg" alt="product" />
-            <div className="product__card__button" onClick={() => pushProduct(product)}>
-                <Button text="+" variant="secondary"/>
-            </div>
+            <img src={product.image} alt="product" />
+            <Button text="+" variant="secondary" buttonFunction={() => pushProduct(product)}/>
         </div>
     )
 }
