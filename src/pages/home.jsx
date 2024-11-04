36e333107f267e98351db7d599123f5b9f025909
Jakub Keklak <keklak.jakub122@gmail.com>
2024-11-04 21:48:20 +0100

    Refactor Home page layout to include ServicesBlock, ContentBlock, and FaqsBlock components for improved structure and content organization

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -8,33 +8,29 @@ import BlogWrapper from '../components/BlogWrapper';
 import BlogCard from '../components/BlogCard';
 import ColoredWrapper from '../components/coloredWrapper';
 import Gallery from '../components/gallery'; 
+import ContentBlock from '../components/ContentBlock';
+import FaqsBlock from '../components/FaqsBlock';
+import ServicesBlock from '../components/ServicesBlock';
 
 const Home = () => {
 
     return (
-        <div className="hero-container" >
-            <AnimatedHero data={slides} />
-            <ColoredWrapper>
-                <div className="container">
-                    <h2>Naše služby</h2>
-                    <Text>
-                        Drevo ako palivo vo všetkých svojich formách, je prírodný zdroj energie. 
-                        Pri jeho správnom spaľovaní a pri správnej vlhkosti horí drevo takmer bez dymu, ľahko sa zapaľuje a 
-                        tvorí málo popola. Následne drevný popol sa výborne hodí ako prírodné hnojivo. Najdlhšie oheň udržiava 
-                        tvrdé drevo, najľahšiu zápalnosť má mäkké drevo, z ktorého sa vyrábajú aj podpaľovacie triesky. 
-                        <br /> <br />
-                        V súčasnosti, keď sa ekologické alternatívy stávajú čoraz dôležitejšími, vidíme nárast záujmu o 
-                        obnoviteľné zdroje energie, ako je drevo, a zároveň o inovácie v iných oblastiach, vrátane online zábavy. 
-                        Nové trendy sa objavujú nielen v ekologických riešeniach, ale aj v digitálnom svete. Hráči na Slovensku čoraz 
-                        častejšie hľadajú nove online casino Slovensko, ktoré ponúkajú inovatívne herné zážitky, bonusy a bezpečné platformy. 
-                        Moderné kasína tak využívajú nové technológie a zároveň sa orientujú na udržateľnosť v oblasti prevádzky a digitálnej
-                        zábavy.
-                    </Text>
-                </div>
-            </ColoredWrapper>
+        <div className="home" >
+            <AnimatedHero data={slides} image="https://freedomfirewood.com/wp-content/uploads/packaged-firewood.jpg"/>
+
+            <ServicesBlock />
+            
+            <ContentBlock />
+
+            <FaqsBlock />
+
+            <ContentBlock  imageRight={true}/>
+
             <ColoredWrapper color='gray'>
                 <Gallery variant="row" data={slides} />
             </ColoredWrapper>
+
+
             <ColoredWrapper>
                 <div className="container">
                     <BlogWrapper
