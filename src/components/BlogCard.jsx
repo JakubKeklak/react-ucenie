10d43bddebe29f7775dc19c55a011c05b31c01b4
Jakub Keklak <keklak.jakub122@gmail.com>
2024-11-04 21:46:36 +0100

    Refactor BlogCard component and CSS for improved layout and hover effects

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -3,10 +3,12 @@ import './BlogCard.css';
 const BlogCard = ({ blog, index }) => {
     return (
         <div className='blogcard' key={index}>
-        <img src={blog.image} alt={blog.title} />
-        <h3>{blog.title}</h3>
-        <p>{blog.text}</p>
-        <span>{blog.category}</span>
+            <img src={blog.image} alt={blog.title} />
+            <div className='blogcard__content'>
+                <h3>{blog.title}</h3>
+                <p>{blog.text}</p>
+                <span  className='blogcard__bottom'>{blog.category}</span>
+            </div>
         </div>
     );
     }
