06918798d978c56a5a8424a8166e1b125a39f95c
Jakub Keklak <keklak.jakub122@gmail.com>
2024-11-13 16:48:28 +0100

    Add disabled state to Button component and update styles for better UX

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -3,18 +3,18 @@ import { HashLink } from 'react-router-hash-link';
 import { Link } from 'react-router-dom';
 import { ReactComponent as ArrowRight } from '../svg/rightArrow.svg';
 
-const Button = ({ text, url, variant, icon, submit, buttonFunction,activeButton }) => {
+const Button = ({ text, url, variant, icon, submit, buttonFunction,activeButton, disabledButton }) => {
   const classVariant = variant || 'primary';
   const buttonIcon = icon || false;
   const isSubmit = submit || false;
-  
+  const disabled = disabledButton ? true : undefined;
   const isHashLink = url && url.includes('#');
   const LinkComponent = isHashLink ? HashLink : Link;
   const Tag = url ? LinkComponent : 'button';
   
 
   return (
-    <Tag to={url} type={isSubmit ? "submit" : undefined} className={`button button--${classVariant} ${activeButton}`} smooth={isHashLink ? true : undefined} onClick={buttonFunction}>
+    <Tag to={url} type={isSubmit ? "submit" : undefined} className={`button button--${classVariant} ${activeButton}`} smooth={isHashLink ? true : undefined} onClick={buttonFunction} disabled={disabled} >
       <span>{text}</span>
       {buttonIcon && 
         <span className="button__icon">
