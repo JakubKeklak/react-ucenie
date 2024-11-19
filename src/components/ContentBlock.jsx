eace20bda37993727f1a0832825e02e4824baa3a
Jakub Keklak <keklak.jakub122@gmail.com>
2024-11-19 20:07:57 +0100

    Enhance ContentBlock styles with heading decoration; adjust image overlay dimensions for improved visual hierarchy

--- src/components/ContentBlock.jsx
+++ src/components/ContentBlock.jsx
@@ -3,13 +3,15 @@ import Heading from './typography/Heading';
 import Text from './text';
 import Image from './parts/Image';
 
-const ContentBlock = ({ children,imageRight }) => {
+const ContentBlock = ({ children, imageRight }) => {
     const variant = imageRight ? 'content-block--image-right' : '';
     return (
         <div className={`content-block ${variant} `}>
             <div className='content-block__wrapper container'>
                 <div className='content-block__content'>
-                    <Heading level={2}>Title</Heading>
+                    <div className='content-block__heading'>
+                        <Heading level={2}>Title</Heading>
+                    </div>
                     <Text>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                         Nulla convallis libero nec metus convallis, auctor tincidunt
@@ -25,7 +27,7 @@ const ContentBlock = ({ children,imageRight }) => {
                         Nulla convallis libero nec metus convallis, auctor tincidunt
                         nunc fermentum. Nullam nec nunc nec nunc fermentum.
                     </Text>
-                    
+
                 </div>
                 <div className='content-block__image'>
                     <Image src="https://urobsisam.zoznam.sk/wp-content/uploads/2024/09/stiepacka-dreva-1.jpg" alt="" />
