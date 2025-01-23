fd1b9cfc87c4d027c5271cd26b176fcc438b4cdc
JakubKeklak <keklak.jakub122@gmail.com>
2025-01-23 11:22:29 -0800

    Updates

---
+++ static/js/423.96479516.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_ucenie=self.webpackChunkreact_ucenie||[]).push([[423],{423:(c,e,t)=>{t.d(e,{pte:()=>r});var a=t(3441);function r(c){return(0,a.k5)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{d:"M256 294.1L383 167c9.4-9.4 24.6-9.4 33.9 0s9.3 24.6 0 34L273 345c-9.1 9.1-23.7 9.3-33.1.7L95 201.1c-4.7-4.7-7-10.9-7-17s2.3-12.3 7-17c9.4-9.4 24.6-9.4 33.9 0l127.1 127z"},child:[]}]})(c)}}}]);
+//# sourceMappingURL=423.96479516.chunk.js.map
